7734701324abfe9e5f8f681d94e682b93208780c
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-08-05 23:34:48 +0900

    top components 추가

---
+++ components/Top.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Topstyles from '../styles/Top.module.css'
+
+export default function Top() {
+  return (
+    <div className={Topstyles.top}>
+      <Link href="/">
+        <a>
+          <Image
+            style={{ margin: 50 }}
+            width='200'
+            height='100'
+            src="/images/Youtube-Channel-Art-2.png"
+            alt="test"
+          />
+        </a>
+      </Link>
+      <h2>Instruction page</h2>
+    </div>
+  )
+}
