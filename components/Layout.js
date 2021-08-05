0dcbf2583370dc87a484240eef5d12e1f7c5b105
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-08-05 23:34:14 +0900

    주석 추가

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,14 @@
 import Nav from './Nav'
 import Head from 'next/head'
+import Top from './Top'
 
 const Layout = ({children}) => {
   return (
     <>
+      <Top />
       <Nav />
       <div>
+        {/* index.js 및 photos.js가 들어감 */}
         {children}   
       </div>
 
